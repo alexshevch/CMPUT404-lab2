1d19231b2c733d5a915543c603af6d51db063a1f
Alex Shevchenko <shevaroller@gmail.com>
2016-09-14 15:01:14 -0600

    forward client to google

--- lab2-server.py
+++ lab2-server.py
@@ -2,8 +2,10 @@ import socket
 
 # Allocate a new socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Magic line we add to be able to reuse ports
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# isten onport 8000
+# Listen on port 8000
 server.bind(('0.0.0.0', 8000))
 server.listen(1)
 
@@ -12,3 +14,15 @@ client, address = server.accept()
 print "Connected!"
 print address
 
+# client is going to be curl, web browser, etc.
+outgoing = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+outgoing.connect(('www.google.ca', 80))
+while True:
+    part = client.recv(1024)
+    print "<" + part
+    if part:
+        outgoing.sendall(part)
+    part = outgoing.recv(1024)
+    print "> " + part
+    if part:
+        client.sendall(part)
